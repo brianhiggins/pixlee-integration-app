33911e26ab792ffe0659e2a7f5ebd1f7c2998d42
Brian Higgins <brian@pixleeteam.com>
2015-08-02 12:54:46 -0700

    no gon butt

--- app/controllers/pixlee_controller.rb
+++ app/controllers/pixlee_controller.rb
@@ -13,13 +13,13 @@ class PixleeController < ApplicationController
 
   def createAuth
   	$auth_token = ($user_id + params[:timeStamp] + params[:url] + "pixlee")
-  	p $auth_token
   	$auth_token = Digest::SHA1.hexdigest($auth_token)
   	p $auth_token
 
   end
 
   def getAuth
+  	p $auth_token
   	respond_to do |format|
 		format.json {render json: $auth_token}
  	end
