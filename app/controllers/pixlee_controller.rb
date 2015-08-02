d45d588d2072c303c1eada0b551a7abab38896c0
Brian Higgins <brian@pixleeteam.com>
2015-08-02 12:44:10 -0700

    no gon butt

--- app/controllers/pixlee_controller.rb
+++ app/controllers/pixlee_controller.rb
@@ -1,3 +1,5 @@
+require 'digest/SHA1'
+
 class PixleeController < ApplicationController
   def start
   end
@@ -10,9 +12,9 @@ class PixleeController < ApplicationController
   end
 
   def createAuth
-  	$auth_token = ($user_id + params[:data][:timeStamp] + params[:data][:url] + "pixlee")
+  	$auth_token = ($user_id + params[:timeStamp] + params[:url] + "pixlee")
   	p $auth_token
-  	$auth_token = Digest::SHA2.new(512).digest($auth_token)
+  	$auth_token = Digest::SHA1.new(512).digest($auth_token)
   	p $auth_token
 
   end
