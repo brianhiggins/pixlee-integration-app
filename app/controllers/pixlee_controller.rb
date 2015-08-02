bc9d443c85b5f993f3294517058bdfde334c60a6
Brian Higgins <brian@pixleeteam.com>
2015-08-02 12:27:33 -0700

    no gon butt

--- app/controllers/pixlee_controller.rb
+++ app/controllers/pixlee_controller.rb
@@ -3,14 +3,14 @@ class PixleeController < ApplicationController
   end
 
   def index
-  	if @user_id == nil
-	  	@user_id = params[:uid]
+  	if $user_id == nil
+	  	$user_id = params[:uid]
   	end
   end
 
   def createAuth
-  	p @user_id
-  	@auth_token = (@user_id + params[:timeStamp] + params[:url] + "pixlee")
+  	p $user_id
+  	@auth_token = ($user_id + params[:timeStamp] + params[:url] + "pixlee")
   	p @auth_token
   	@auth_token = Digest::SHA2.new(512).hexdigest(@auth_token)
   	p @auth_token
