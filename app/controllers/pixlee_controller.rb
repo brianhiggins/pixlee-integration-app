d2ae7d0f69eaf5e107c4aae09c6091bad894dd72
Brian Higgins <brian@pixleeteam.com>
2015-08-03 13:28:38 -0700

    return ajax

--- app/controllers/pixlee_controller.rb
+++ app/controllers/pixlee_controller.rb
@@ -11,7 +11,7 @@ class PixleeController < ApplicationController
   def createAuth
   	$auth_token = ($user_id + params[:timeStamp] + params[:url] + "pixlee")
   	p $auth_token
-  	$auth_token = Digest::SHA512($auth_token)
+  	$auth_token = Digest::SHA512.digest($auth_token)
   end
 
   def getAuth
