1c66c79f7735fadf5b18a43507ec8ebabc309529
Brian Higgins <brian@pixleeteam.com>
2015-08-02 19:14:33 -0700

    no gon butt

--- app/assets/javascripts/hootsuite_helper.js
+++ app/assets/javascripts/hootsuite_helper.js
@@ -31,7 +31,7 @@ function authenticate(pic_url, time) {
 			data: {},
 			dataType: "string",
 		}).always(function(resp) {
-			console.log(resp[0] + " end. ");
+			console.log(resp[$auth_token] + " end. ");
 			 window.alert(resp);
 		})
 	);
