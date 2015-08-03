fd5b680f2eb2824e034d47a1f2b3af5d552f0f12
Brian Higgins <brian@pixleeteam.com>
2015-08-03 14:57:21 -0700

    sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_pixlee-integration-app_session', :sharedSecret => "pixlee"
-
+Rails.application.config.session_store :cookie_store, key: '_pixlee-integration-app_session' 
