67f995ac1e771453cf36a2945f774a17a00e797a
Brian Higgins <brian@pixleeteam.com>
2015-08-03 14:08:42 -0700

    return ajax

--- app/controllers/pixlee_controller.rb
+++ app/controllers/pixlee_controller.rb
@@ -2,22 +2,24 @@ require 'digest/sha1'
 
 class PixleeController < ApplicationController
   def start
+  	session[:test] = "test"
   end
 
   def index
+  	p session[:test]
   	session[:user_id] = params[:uid]
-  	p session[:user_id]
+  	# p session[:user_id]
   end
 
   def createAuth
-  	p session[:user_id]
+  	# p session[:user_id]
   	$auth_token = (session[:user_id] + params[:timeStamp] + params[:url] + "pixlee")
-  	p $auth_token
+  	# p $auth_token
   	$auth_token = Digest::SHA512.digest($auth_token)
   end
 
   def getAuth
-  	p $auth_token
+  	# p $auth_token
   	respond_to do |format|
 		format.json {render json: $auth_token}
  	end
