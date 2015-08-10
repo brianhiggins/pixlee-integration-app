df8151c07185023f7866d1956fbf93b074208900
Brian Higgins <brian@pixleeteam.com>
2015-08-10 13:05:13 -0700

    time

--- app/assets/javascripts/hootsuite_helper.js
+++ app/assets/javascripts/hootsuite_helper.js
@@ -1,12 +1,22 @@
 function attach(pic_url, pic_name, pic_extension) {
+	var _this = this;
+	this.timeStamp = 0;
+
+	function saveTime (loginTimeStamp) {
+		this.timeStamp = loginTimeStamp;
+	}
+
 	funciton() {
 		$.ajax({
 			url: "getTime",
 			type: "GET",
 			data: {},
 			dataType: "json"
-		})
-	}
+		}).success( function(resp) {
+			saveTime(resp.responseText);
+		});
+	};
+
 	// calls controller function that will create the token to allow the post in
 	// hootsuite's backend
 	$.ajax({
@@ -24,7 +34,7 @@ function attach(pic_url, pic_name, pic_extension) {
 			url: pic_url, 
 			name: pic_name,
 			extension: pic_extension, 
-			timestamp: time, 
+			timestamp: _this.timeStamp, 
 			token: resp.responseText
 		});
 	});
