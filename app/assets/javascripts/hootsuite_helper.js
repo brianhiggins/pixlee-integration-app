114b239557ffdf94e3f39426a3d0ff7d98dc20f3
Brian Higgins <brian@pixleeteam.com>
2015-08-10 13:57:15 -0700

    time

--- app/assets/javascripts/hootsuite_helper.js
+++ app/assets/javascripts/hootsuite_helper.js
@@ -25,20 +25,21 @@ function attach(pic_url, pic_name, pic_extension) {
 	}).success( function(resp) {
 		console.log(resp);
 		saveTime(resp);
-	});
+		
+		// calls controller function that will create the token to allow the post in
+		// hootsuite's backend
+		$.ajax({
+			url: "getAuth",
+			type: "GET",
+			data: {
+				url: pic_url,
+			},
+			dataType: "json",
+		}).success(function(resp) { // gets the auth token as resp.responseText
+			console.log(resp.responseText);
+			// uses hootsuite api function to attach file
+			sentAttachRequest(resp.responseText);
+		}).fail(console.log("fail"));
 
-	// calls controller function that will create the token to allow the post in
-	// hootsuite's backend
-	$.ajax({
-		url: "getAuth",
-		type: "GET",
-		data: {
-			url: pic_url,
-		},
-		dataType: "json",
-	}).success(function(resp) { // gets the auth token as resp.responseText
-		console.log(resp.responseText);
-		// uses hootsuite api function to attach file
-		sentAttachRequest(resp.responseText);
-	}).fail(console.log("fail"));
+	});
 }
