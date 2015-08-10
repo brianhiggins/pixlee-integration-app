f8ba9b281219598695d04c94ca5c18369eca69ee
Brian Higgins <brian@pixleeteam.com>
2015-08-10 10:49:34 -0700

    time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception, :except => :pixlee
+  protect_from_forgery with: :exception
   
   protect_from_forgery :shared_secret => "pixlee", :except => :pixlee
  
