787b83036379dad1ea980a875baf298e264a8e56
Brian Higgins <brian@pixleeteam.com>
2015-08-10 10:32:37 -0700

    time

--- app/controllers/pixlee_controller.rb
+++ app/controllers/pixlee_controller.rb
@@ -11,12 +11,13 @@ class PixleeController < ApplicationController
 
   # creates the token for attaching a file and returns it to an ajax get request
   def getAuth
-    p session[:user_id]
+    p session[:timeStamp]
   	auth_token = session[:user_id] + session[:timeStamp] + params[:url] + "pixlee"
   	auth_token = Digest::SHA512.digest(auth_token)
 
-    value = [auth_token, timeStamp]
+    value = [auth_token, session[:timeStamp]]
     p value
+
   	respond_to do |format|
   		format.json {render json: value}
    	end
